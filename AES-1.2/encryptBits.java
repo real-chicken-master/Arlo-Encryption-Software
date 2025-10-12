d4ec8e756932c3eec5b6294b29ef361a307848b3
real-chicken-master <aomkennedy@gmail.com>
2025-10-13 10:39:33 +1300

    started new encrypt method

---
+++ AES-1.2/encryptBits.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class encryptBits here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class encryptBits
+{
+    // instancevariables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class encryptBits
+     */
+    public static String[] encryptBits(String[] input)
+    {
+        // initialise instance variables
+        String[] output= input;
+        return output;
+    }
+}
