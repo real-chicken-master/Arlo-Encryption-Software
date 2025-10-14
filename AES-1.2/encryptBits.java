c6a56ae55414aead699b29127028d6c685bee70c
real-chicken-master <aomkennedy@gmail.com>
2025-10-14 13:04:11 +1300

    did work on encrypt bits

--- AES-1.2/encryptBits.java
+++ AES-1.2/encryptBits.java
@@ -7,15 +7,61 @@
  */
 public class encryptBits
 {
-    // instancevariables - replace the example below with your own
-
-    /**
-     * Constructor for objects of class encryptBits
-     */
-    public static String[] encryptBits(String[] input)
+    public static String[] encryptBits(String input)
     {
-        // initialise instance variables
-        String[] output= input;
+        String key = randomKey();
+        String output = input;
+        System.out.println(encrypt2dArray(output, key.toCharArray()));
+        String[] outputArray = {output, key};
+        return outputArray;
+    }
+
+    private static String encrypt2dArray(String input,char[] key){
+        String output = input;
+        int xLength = (int)Math.ceil(Math.sqrt(input.length()));
+        int yLength = xLength;
+        char[] tempArray = output.toCharArray();
+        char[][] tempArray2 = new char[xLength][yLength];
+        int num = 0;
+        System.out.println(tempArray);
+        System.out.println(rotate(tempArray,9));
+        for(int x = 0; x < xLength; x++){
+            for(int y = 0; y < yLength; y++){
+                if(num < tempArray.length){
+                    tempArray2[x][y]=tempArray[num];
+                }
+                num++;
+            }
+        }
+        for(num = 0; num < (key.length/2); num++){
+            char[][] tempArray3 = new char[xLength][yLength];
+            int num2 = 0;
+            
+            tempArray2 = tempArray3;
+        }
         return output;
     }
+    
+    private static char[] rotate(char[] input, int num){
+        char[] output = new char[input.length];
+        for(int num2 = 0; num2 < input.length; num2++){
+            int modifier = 0;
+            while(num2+num-modifier >= input.length){
+                modifier += input.length;
+            }
+            output[num2] = input[num2+num-modifier];
+        }
+        return output;
+    }
+    
+    private static String randomKey(){
+        String output = "";
+        int num = 0;
+        for(int x = 0; x < 12; x++){
+            num = (int)(Math.random()*9);
+            output += num;
+        }
+        return output;
+    }
+
 }
