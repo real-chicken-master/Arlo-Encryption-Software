eefc3d8775252638e9a1973d462fdf2c4fbbfcf6
real-chicken-master <aomkennedy@gmail.com>
2025-11-20 09:14:40 +1300

    started to add logic to make the windows generate in a new tab

--- AES-2.2/GuiManager.java
+++ AES-2.2/GuiManager.java
@@ -4,10 +4,26 @@ import java.util.List;
 import java.awt.Frame;
 import java.lang.reflect.Method;
 import javax.swing.JOptionPane;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 public class GuiManager {
     private static final List<JFrame> openFrames = new ArrayList<>();
 
     public static void registerFrame(JFrame frame) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+        Dimension screenSize = toolkit.getScreenSize();
+
+        double screenWidth = screenSize.getWidth();
+        double screenHeight = screenSize.getHeight();
+        int x = 500*openFrames.size()-8;
+        if(x+500 <= screenWidth){
+            frame.setLocation(x, 0);
+        }else{
+            if(500 <= screenHeight){
+                frame.setLocation(0, 500);
+            }
+        }
         openFrames.add(frame);
     }
 
